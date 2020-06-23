d639e8a700a149e1ff155d941aa62a3c6b4a2821
Antoine Khoury <Antoine.Khoury@scai.intra>
2020-06-23 22:26:52 +0200

    sending  msgs from messenger component

--- src/app/messenger/messenger.component.ts
+++ src/app/messenger/messenger.component.ts
@@ -17,49 +17,71 @@ export class MessengerComponent implements OnInit {
   ) {}
   users = this.auth.localStorageArray;
   currentUser = JSON.parse(localStorage.getItem('key'));
-  // messages = this.auth.localStorageArray[
-  //   JSON.parse(localStorage.getItem('key'))
-  // ].messages;
+  id = null;
+  inputData;
+  messageTo;
+  sentMessage;
   messages = this.auth.localStorageArray[this.currentUser].messages;
   navigate(id) {
-    this.route.navigate(['/add-friends', id]);
+    this.id = id;
   }
-  // image() {
-  //   return this.storageService.getImage();
-  // }
-  // // id;
-  // new;
-
-  // reply(data, id) {
-  //   let newMessage = this.auth.localStorageArray[id].messages;
-  //   // console.log(newMessage);
-  //   newMessage.push({
-  //     image: this.auth.localStorageArray[this.currentUser].image,
-  //     message: data.value,
-  //   });
-
-  //   this.new = {
-  //     image: this.auth.localStorageArray[id].image,
-  //     name: this.auth.localStorageArray[id].name,
-  //     lastName: this.auth.localStorageArray[id].lastName,
-  //     email: this.auth.localStorageArray[id].email,
-  //     password: this.auth.localStorageArray[id].password,
-  //     confirmPassword: this.auth.localStorageArray[id].confirmPassword,
-  //     gender: this.auth.localStorageArray[id].gender,
-  //     study: this.auth.localStorageArray[id].study,
-  //     wentTo: this.auth.localStorageArray[id].wentTo,
-  //     livesIn: this.auth.localStorageArray[id].livesIn,
-  //     from: this.auth.localStorageArray[id].from,
-  //     messages: newMessage,
-  //     posts: this.auth.localStorageArray[id].posts,
-  //   };
-
-  //   this.auth.localStorageArray[id] = this.new;
-
-  //   localStorage.setItem('user', JSON.stringify(this.auth.localStorageArray));
-  // }
-
-  ngOnInit() {
-    // this.id = parseInt(this.aroute.snapshot.paramMap.get('id'));
+  open(id) {
+    this.id = id;
   }
+  send(id, data) {
+    let newMessage = this.auth.localStorageArray[id].messages;
+
+    newMessage.push({
+      image: this.auth.localStorageArray[this.currentUser].image,
+      message: data,
+      id: this.currentUser,
+    });
+
+    let userMessage = this.auth.localStorageArray[this.currentUser].messages;
+    userMessage.push({
+      image: this.auth.localStorageArray[this.currentUser].image,
+      message: data,
+      to: this.auth.localStorageArray[id].image,
+    });
+    this.messageTo = {
+      image: this.auth.localStorageArray[id].image,
+      name: this.auth.localStorageArray[id].name,
+      lastName: this.auth.localStorageArray[id].lastName,
+      email: this.auth.localStorageArray[id].email,
+      password: this.auth.localStorageArray[id].password,
+      confirmPassword: this.auth.localStorageArray[id].confirmPassword,
+      gender: this.auth.localStorageArray[id].gender,
+      study: this.auth.localStorageArray[id].study,
+      wentTo: this.auth.localStorageArray[id].wentTo,
+      livesIn: this.auth.localStorageArray[id].livesIn,
+      from: this.auth.localStorageArray[id].from,
+      messages: newMessage,
+      posts: this.auth.localStorageArray[id].posts,
+    };
+
+    this.sentMessage = {
+      image: this.auth.localStorageArray[this.currentUser].image,
+      name: this.auth.localStorageArray[this.currentUser].name,
+      lastName: this.auth.localStorageArray[this.currentUser].lastName,
+      email: this.auth.localStorageArray[this.currentUser].email,
+      password: this.auth.localStorageArray[this.currentUser].password,
+      confirmPassword: this.auth.localStorageArray[this.currentUser]
+        .confirmPassword,
+      gender: this.auth.localStorageArray[this.currentUser].gender,
+      study: this.auth.localStorageArray[this.currentUser].study,
+      wentTo: this.auth.localStorageArray[this.currentUser].wentTo,
+      livesIn: this.auth.localStorageArray[this.currentUser].livesIn,
+      from: this.auth.localStorageArray[this.currentUser].from,
+      messages: userMessage,
+      posts: this.auth.localStorageArray[this.currentUser].posts,
+    };
+
+    this.auth.localStorageArray[id] = this.messageTo;
+    this.auth.localStorageArray[this.currentUser] = this.sentMessage;
+    localStorage.setItem('user', JSON.stringify(this.auth.localStorageArray));
+    this.inputData = null;
+    this.id = null;
+  }
+
+  ngOnInit() {}
 }
