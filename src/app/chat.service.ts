7a080a81e3588ac69b4046c872d86b14ff467036
Antoine Khoury <Antoine.Khoury@scai.intra>
2020-06-23 22:02:11 +0200

    home page messaging feature completed

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,101 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { PostsService } from '../posts.service';
-import { Router } from '@angular/router';
-import { AuthService } from '../log-in/auth.service';
-import { StorageService } from '../storage.service';
-import { ChatService } from '../chat.service';
+import { Injectable } from '@angular/core';
+import { AuthService } from './log-in/auth.service';
 
-@Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
+@Injectable({
+  providedIn: 'root',
 })
-export class HomePageComponent implements OnInit {
-  posts = [];
-  userFriends = JSON.parse(localStorage.getItem('user'));
-  currentUserId = JSON.parse(localStorage.getItem('key'));
+export class ChatService {
+  constructor(private auth: AuthService) {}
 
-  data;
-  constructor(
-    private postService: PostsService,
-    private route: Router,
-    private auth: AuthService,
-    private storageService: StorageService,
-    private chat: ChatService
-  ) {}
-  likeBtn = false;
-  input;
-  foto;
-  preview;
-  posted = false;
-  editedComment;
-  inputData;
-  id;
-  show = false;
   users = JSON.parse(localStorage.getItem('user'));
+  sender;
   currentUser = JSON.parse(localStorage.getItem('key'));
   messageTo;
   sentMessage;
-
-  getUserName() {
-    return this.storageService.getName();
-  }
-  goToDescription(id) {
-    this.route.navigate(['/description', id]);
-  }
-  post(data) {
-    this.postService.post(data, this.foto);
-    this.input = undefined;
-    this.posted = true;
-  }
-  image() {
-    return this.storageService.getImage();
-  }
-
-  uploadImage(event) {
-    if (event.target.files) {
-      let reader = new FileReader();
-      reader.readAsDataURL(event.target.files[0]);
-      reader.onload = (event) => {
-        this.foto = event.target.result;
-        this.preview = this.foto;
-      };
-    }
-  }
-
-  like(id) {
-    this.postService.like(id);
-  }
-
-  showComments(id) {
-    this.postService.showComment(id);
-  }
-  commentLike(postId, commentId) {
-    if (this.postService.posts[postId].comments[commentId].likePressed) {
-      this.postService.commentDisLike(postId, commentId);
-    } else {
-      this.postService.commentLike(postId, commentId);
-    }
-  }
-
-  edit(postId, commentId, editedComment) {
-    this.postService.edit(postId, commentId, editedComment);
-  }
-
-  comment(id, comment) {
-    this.postService.comment(id, comment.value);
-    comment.value = '';
-  }
-  removeComment(postId, commentId) {
-    this.postService.removeComment(postId, commentId);
-  }
-  share(id) {
-    this.postService.share(id);
-  }
-  open(id) {
-    this.id = id;
-    this.show = !this.show;
-  }
 
   sendTo(id, data) {
     let newMessage = this.auth.localStorageArray[id].messages;
@@ -148,10 +64,5 @@ export class HomePageComponent implements OnInit {
     this.auth.localStorageArray[id] = this.messageTo;
     this.auth.localStorageArray[this.currentUser] = this.sentMessage;
     localStorage.setItem('user', JSON.stringify(this.auth.localStorageArray));
-    this.inputData = null;
-  }
-
-  ngOnInit() {
-    this.posts = this.postService.posts;
   }
 }
